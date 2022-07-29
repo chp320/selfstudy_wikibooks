d59078ca1f5ba02a718891386f0044d74654841d
415969 <a415969@415969-AOA4012100007.local>
2022-07-29 18:16:03 +0900

    Initial commit

---
+++ src/main/java/com/selfstudy/wikibooks/WikibooksApplication.java
@@ -0,0 +1,13 @@
+package com.selfstudy.wikibooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikibooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikibooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfstudy/wikibooks/WikibooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfstudy.wikibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikibooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
